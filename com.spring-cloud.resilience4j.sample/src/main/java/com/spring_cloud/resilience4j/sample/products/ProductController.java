3dd042143da57a0171fa240aa74f34c49cc13a99
jvn1o <junok0987@naver.com>
2025-09-15 19:29:56 +0900

    서킷 브레이커 (CircuitBreaker) 사용 예제

---
+++ com.spring-cloud.resilience4j.sample/src/main/java/com/spring_cloud/resilience4j/sample/products/ProductController.java
@@ -0,0 +1,18 @@
+package com.spring_cloud.resilience4j.sample.products;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/product/{productId}")
+    public Product getProduct(@PathVariable String productId) {
+        return productService.getProductDetails(productId);
+    }
+}

7533ed82e8a52c5946cb7d5d03fbbd95d3dcea68
jvn1o <junok0987@naver.com>
2025-09-17 14:08:21 +0900

    정적 페이지와 동적 페이지

---
+++ com.sparta.spring-mvc/src/test/java/com/sparta/spring_mvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

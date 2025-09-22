0083ba5555c22c883cf184f1c6eddf01bf27d559
jvn1o <junok0987@naver.com>
2025-09-22 23:42:55 +0900

    영속성 컨텍스트

--- com.sparta.jpa-core/src/test/java/EntityTest.java
+++ com.sparta.jpa-core/src/test/java/EntityTest.java
@@ -1,7 +1,10 @@
+import com.sparta.entity.Memo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class EntityTest {
@@ -16,7 +19,197 @@ public class EntityTest {
     }
 
     @Test
+    @DisplayName("EntityTransaction 성공 테스트")
     void test1() {
+        EntityTransaction et = em.getTransaction(); // EntityManager 에서 EntityTransaction 을 가져옵니다.
 
+        et.begin(); // 트랜잭션을 시작합니다.
+
+        try { // DB 작업을 수행합니다.
+
+            Memo memo = new Memo(); // 저장할 Entity 객체를 생성합니다.
+            memo.setId(1L); // 식별자 값을 넣어줍니다.
+            memo.setUsername("Robbie");
+            memo.setContents("영속성 컨텍스트와 트랜잭션 이해하기");
+
+            em.persist(memo); // EntityManager 사용하여 memo 객체를 영속성 컨텍스트에 저장합니다.
+
+            et.commit(); // 오류가 발생하지 않고 정상적으로 수행되었다면 commit 을 호출합니다.
+            // commit 이 호출되면서 DB 에 수행한 DB 작업들이 반영됩니다.
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            et.rollback(); // DB 작업 중 오류 발생 시 rollback 을 호출합니다.
+        } finally {
+            em.close(); // 사용한 EntityManager 를 종료합니다.
+        }
+
+        emf.close(); // 사용한 EntityManagerFactory 를 종료합니다.
+    }
+
+    @Test
+    @DisplayName("Entity 조회 : 캐시 저장소에 해당하는 Id가 존재하지 않은 경우")
+    void test2() {
+        try {
+
+            Memo memo = em.find(Memo.class, 1);
+            System.out.println("memo.getId() = " + memo.getId());
+            System.out.println("memo.getUsername() = " + memo.getUsername());
+            System.out.println("memo.getContents() = " + memo.getContents());
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    @Test
+    @DisplayName("Entity 조회 : 캐시 저장소에 해당하는 Id가 존재하는 경우")
+    void test3() {
+        try {
+
+            Memo memo1 = em.find(Memo.class, 1);
+            System.out.println("memo1 조회 후 캐시 저장소에 저장\n");
+
+            Memo memo2 = em.find(Memo.class, 1);
+            System.out.println("memo2.getId() = " + memo2.getId());
+            System.out.println("memo2.getUsername() = " + memo2.getUsername());
+            System.out.println("memo2.getContents() = " + memo2.getContents());
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    @Test
+    @DisplayName("객체 동일성 보장")
+    void test4() {
+        EntityTransaction et = em.getTransaction();
+
+        et.begin();
+
+        try {
+            Memo memo3 = new Memo();
+            memo3.setId(2L);
+            memo3.setUsername("Robbert");
+            memo3.setContents("객체 동일성 보장");
+            em.persist(memo3);
+
+            Memo memo1 = em.find(Memo.class, 1);
+            Memo memo2 = em.find(Memo.class, 1);
+            Memo memo  = em.find(Memo.class, 2);
+
+            System.out.println(memo1 == memo2);
+            System.out.println(memo1 == memo);
+
+            et.commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            et.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    @Test
+    @DisplayName("Entity 삭제")
+    void test5() {
+        EntityTransaction et = em.getTransaction();
+
+        et.begin();
+
+        try {
+
+            Memo memo = em.find(Memo.class, 2);
+
+            em.remove(memo);
+
+            et.commit();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            et.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    @Test
+    @DisplayName("쓰기 지연 저장소 확인")
+    void test6() {
+        EntityTransaction et = em.getTransaction();
+
+        et.begin();
+
+        try {
+            Memo memo = new Memo();
+            memo.setId(2L);
+            memo.setUsername("Robbert");
+            memo.setContents("쓰기 지연 저장소");
+            em.persist(memo);
+
+            Memo memo2 = new Memo();
+            memo2.setId(3L);
+            memo2.setUsername("Bob");
+            memo2.setContents("과연 저장을 잘 하고 있을까?");
+            em.persist(memo2);
+
+            System.out.println("트랜잭션 commit 전");
+            et.commit();
+            System.out.println("트랜잭션 commit 후");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            et.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    @Test
+    @DisplayName("flush() 메서드 확인")
+    void test7() {
+        EntityTransaction et = em.getTransaction();
+
+        et.begin();
+
+        try {
+            Memo memo = new Memo();
+            memo.setId(4L);
+            memo.setUsername("Flush");
+            memo.setContents("Flush() 메서드 호출");
+            em.persist(memo);
+
+            System.out.println("flush() 전");
+            em.flush(); // flush() 직접 호출
+            System.out.println("flush() 후\n");
+
+
+            System.out.println("트랜잭션 commit 전");
+            et.commit();
+            System.out.println("트랜잭션 commit 후");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            et.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
     }
 }
